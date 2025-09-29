5e458b7da21ff2d8750662d87b1ee5df96ae5388
grem1tgrom <159943954+grem1tgrom@users.noreply.github.com>
2025-09-29 15:01:04 +0300

    fix-FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,39 +1,11 @@
-package ru.yandex.practicum.filmorate.service;
+package ru.yandex.practicum.filmorate.storage;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Service
-public class FilmService {
-    private final FilmStorage filmStorage;
-
-    @Autowired
-    public FilmService(FilmStorage filmStorage) {
-        this.filmStorage = filmStorage;
-    }
-
-    public void addLike(int filmId, int userId) {
-        Film film = filmStorage.getFilmById(filmId);
-        film.getLikes().add(userId);
-    }
-
-    public void removeLike(int filmId, int userId) {
-        Film film = filmStorage.getFilmById(filmId);
-        if (!film.getLikes().contains(userId)) {
-            throw new NotFoundException("Лайк от пользователя с id=" + userId + " не найден.");
-        }
-        film.getLikes().remove(userId);
-    }
-
-    public List<Film> getPopularFilms(int count) {
-        return filmStorage.getAllFilms().stream()
-                .sorted((f1, f2) -> f2.getLikes().size() - f1.getLikes().size())
-                .limit(count)
-                .collect(Collectors.toList());
-    }
+public interface FilmStorage {
+    List<Film> getAllFilms();
+    Film addFilm(Film film);
+    Film updateFilm(Film film);
+    Film getFilmById(int id);
 }
